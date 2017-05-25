f48bdb0d9e6867e4829fccd751aeb247fca9d40f
Valentin Gurmeza <valentin.gurmeza@emcconnected.com>
2017-05-25 14:53:05 -0700

    add integration runner test

---
+++ tests/integration/run_tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd .
+
+cd "$ABSOLUTE_PATH"
+export PYTHONPATH=./pythonlibs/
+ansible-playbook tests.yml --diff -i 127.0.0.1, $@
+
+popd  >/dev/null
